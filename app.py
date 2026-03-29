2877966dc16596248abcc420b91c93a1f1f21f61
2008robocode-cryptoc <2008robocode@gmail.com>
2026-03-29 09:33:45 +0530

    clean minimal gradio fix

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import gradio as gr
 def home():
     return "OpenEnv Email Triage Environment is Running ✅"
 
-iface = gr.Interface(fn=home, inputs=[], outputs="text")
+demo = gr.Interface(fn=home, inputs=[], outputs="text")
 
-iface.launch(server_name="0.0.0.0", server_port=7860)
+demo.launch()
