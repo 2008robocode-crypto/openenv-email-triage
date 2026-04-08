48280a454584d1279a95e586ecfa228c2f517b18
2008robocode-cryptoc <2008robocode@gmail.com>
2026-04-08 21:18:15 +0530

    fix

--- inference.py
+++ inference.py
@@ -6,9 +6,15 @@ import traceback
 from openai import OpenAI
 from core import CustomerSupportEnv
 
-API_KEY = os.environ["API_KEY"]
-API_BASE_URL = os.environ["API_BASE_URL"]
-MODEL_NAME   = os.getenv("MODEL_NAME", "Qwen/Qwen2.5-72B-Instruct")
+try:
+    API_KEY = os.environ["API_KEY"]   # validator
+except KeyError:
+    API_KEY = os.environ.get("HF_TOKEN")  # HF fallback
+
+try:
+    API_BASE_URL = os.environ["API_BASE_URL"]
+except KeyError:
+    API_BASE_URL = "https://router.huggingface.co/v1"
 
 MIN_VAL, MAX_VAL, MAX_STEPS = 0.001, 0.999, 20
 
